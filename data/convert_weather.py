f6108a221640994b2056c79e440d332973f68295
Dmitriy Narkevich <github@dimier.org>
2015-09-26 22:22:47 +0300

    Аггрегация погодных условий - установка дождя, если он был утром или днём.

--- data/convert_weather.py
+++ data/convert_weather.py
@@ -17,6 +17,10 @@ dt_re = re.compile('(\d{1,2})\.(\d{1,2})\.(\d{1,4}) (\d{1,2}):(\d{1,2})')
 reader = csv.reader(sys.stdin, delimiter=',', quotechar='"')
 writer = csv.writer(sys.stdout, delimiter='\t', quotechar='"', lineterminator='\n')
 header = True
+
+rainy_days = {}
+days = []
+
 for row in reader:
     if header:
         writer.writerow(['dt', 'state', 'description_ru', 'description_en', 'temp'])
@@ -32,14 +36,26 @@ for row in reader:
     if len(year) == 2:
         year = '20'+year
 
-    if hour != '15':
-        continue
-
     dt = '{}{:02d}{:02d}'.format(year, int(month), int(day))
 
     conditions = row[3].decode('utf8').lower()
+
     state = ''
     if u'дождь' in conditions or u'морось' in conditions or u'снег' in conditions:
         state = 'rain'
 
-    writer.writerow([dt, state, row[3], row[3], row[1]])
+    if hour in ['6', '9', '12', '15', '18'] and state == 'rain':
+        rainy_days[dt] = row[3]
+
+    if hour == '15':
+        days.append([dt, state, row[3], row[3], row[1]])
+
+
+for row in sorted(days):
+    rainy = rainy_days.get(row[0])
+    if row[1] != 'rain' and rainy:
+        row[1] = 'rain'
+        row[2] = rainy
+        row[3] = rainy
+
+    writer.writerow(row)
