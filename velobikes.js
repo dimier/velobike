4061744faf9d52ec3349010b49be36688d908887
Dmitriy Narkevich <github@dimier.org>
2015-10-06 18:45:18 +0300

    Оформление.

--- velobikes.js
+++ velobikes.js
@@ -241,7 +241,7 @@ ymaps.ready(function () {
     var dateFormat = d3.time.format("%Y%m%d%H"),
         dateFormatDay = d3.time.format("%Y%m%d"), //YYYYMMDDHH date format for acivity calendar
         startDate = dateFormat.parse("2014070300"),
-        endDate = dateFormat.parse("2014110500"),
+        endDate = dateFormat.parse("2014110100"),
         allDays = Math.floor((endDate.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24)),
         tickWidth = Math.floor((window.innerWidth - 40) / allDays);
         tickWidth = tickWidth<3 ? 3 : tickWidth; //setting the minimum tick width (for small screens);//the width of day width
